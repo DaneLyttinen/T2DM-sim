af36048d5274a19db525166214935ea800b1b527
Dane Lyttinen <69489249+DaneLyttinen@users.noreply.github.com>
2023-12-18 18:13:18 +1300

    modified advanced tut for multiprocessing on windows

--- T2DMSimulator/examples/advanced_tut.py
+++ T2DMSimulator/examples/advanced_tut.py
@@ -1,5 +1,4 @@
 import sys
-from pathlib import Path
 
 # Assuming your script is in the 'examples' directory
 sys.path.append('C:\\Users\\Dane\\Desktop\\Masters\\T2DM-sim\\T2DMSimulator')
@@ -17,53 +16,57 @@ from datetime import timedelta
 from datetime import datetime
 
 # specify start_time as the beginning of today
-now = datetime.now()
-start_time = datetime.combine(now.date(), datetime.min.time())
+def main():
+    now = datetime.now()
+    start_time = datetime.combine(now.date(), datetime.min.time())
 
-# --------- Create Random Scenario --------------
-# Specify results saving path
-path = './results'
+    # --------- Create Random Scenario --------------
+    # Specify results saving path
+    path = './results'
 
-# Create a simulation environment
-patient = T2DPatient.withName('adolescent#001')
-sensor = CGMSensor.withName('Dexcom', seed=1)
-pump = InsulinPump.withName('Insulet')
-scenario = RandomScenario(start_time=start_time, seed=1)
-env = T2DSimEnv(patient, sensor, pump, scenario)
+    # Create a simulation environment
+    patient = T2DPatient.withName('adolescent#001')
+    sensor = CGMSensor.withName('Dexcom', seed=1)
+    pump = InsulinPump.withName('Insulet')
+    scenario = RandomScenario(start_time=start_time, seed=1)
+    env = T2DSimEnv(patient, sensor, pump, scenario)
 
-# Create a controller
-controller = BBController()
+    # Create a controller
+    controller = BBController()
 
-# Put them together to create a simulation object
-s1 = SimObj(env, controller, timedelta(days=1), animate=False, path=path)
-results1 = sim(s1)
-print(results1)
+    # Put them together to create a simulation object
+    s1 = SimObj(env, controller, timedelta(days=1), animate=True, path=path)
+    #results1 = sim(s1)
+    #print(results1)
 
-# --------- Create Custom Scenario --------------
-# Create a simulation environment
-patient = T2DPatient.withName('adolescent#001')
-sensor = CGMSensor.withName('Dexcom', seed=1)
-pump = InsulinPump.withName('Insulet')
-# custom scenario is a list of tuples (time, meal_size)
-scen = [(7, 45), (12, 70), (16, 15), (18, 80), (23, 10)]
-scenario = CustomScenario(start_time=start_time, scenario=scen)
-env = T2DSimEnv(patient, sensor, pump, scenario)
+    # --------- Create Custom Scenario --------------
+    # Create a simulation environment
+    patient = T2DPatient.withName('adolescent#001')
+    sensor = CGMSensor.withName('Dexcom', seed=1)
+    pump = InsulinPump.withName('Insulet')
+    # custom scenario is a list of tuples (time, meal_size)
+    scen = [(7, 45), (12, 70), (16, 15), (18, 80), (23, 10)]
+    scenario = CustomScenario(start_time=start_time, scenario=scen)
+    env = T2DSimEnv(patient, sensor, pump, scenario)
 
-# Create a controller
-controller = BBController()
+    # Create a controller
+    controller = BBController()
 
-# Put them together to create a simulation object
-s2 = SimObj(env, controller, timedelta(days=1), animate=False, path=path)
-results2 = sim(s2)
-print(results2)
+    # Put them together to create a simulation object
+    s2 = SimObj(env, controller, timedelta(days=1), animate=True, path=path)
+    #results2 = sim(s2)
+    #print(results2)
 
 
-# --------- batch simulation --------------
-# Re-initialize simulation objects
-s1.reset()
-s2.reset()
+    # --------- batch simulation --------------
+    # Re-initialize simulation objects
+    s1.reset()
+    s2.reset()
 
-# create a list of SimObj, and call batch_sim
-s = [s1, s2]
-results = batch_sim(s, parallel=True)
-print(results)
+    # create a list of SimObj, and call batch_sim
+    s = [s1, s2]
+    results = batch_sim(s, parallel=True)
+    print(results)
+
+if __name__ == '__main__':
+    main()
