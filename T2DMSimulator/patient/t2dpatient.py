Fixed glucose params and bug in calculation, added baseline controller, added params for each subtype

T2DMSimulator/patient/t2dpatient.py
@@ -8,7 +8,8 @@ import pkg_resources
 from T2DMSimulator.glucose.GlucoseDynamics import GlucoseDynamics
 from T2DMSimulator.glucose.GlucoseParameters import GlucoseParameters
 from T2DMSimulator.glucose.glucose_initializer import GlucoseInitializer
-from scipy.integrate import solve_ivp
+from queue import PriorityQueue
+
 logger = logging.getLogger(__name__)
 
 Action = namedtuple("patient_action", ['CHO', 'insulin_fast', 'insulin_long', 'metformin', 'vildagliptin', 'stress', 'physical'])
@@ -31,7 +32,9 @@ class T2DPatient(Patient):
                  GBPC0=None, 
                  IBPF0=None, 
                  brates=None,
-                 glucose_params=None):
+                 glucose_params=None,
+                 name="testing",
+                 prob_of_actioning=100):
         '''
         T2DPatient constructor.
         Inputs:
@@ -42,7 +45,7 @@ class T2DPatient(Patient):
             - t0: simulation start time, it is 0 by default
         '''
         self._params = params
-        self.name = "testing"
+        self.name = name
         self.__param = GlucoseParameters() if glucose_params == None else glucose_params
         self.GBPC0 = 7/0.0555 if GBPC0 is None else GBPC0
         self.IBPF0 = 1 if IBPF0 is None else IBPF0
@@ -51,6 +54,8 @@ class T2DPatient(Patient):
         self.random_init_bg = random_init_bg
         self._seed = seed
         self.t0 = t0
+        self.prob = prob_of_actioning
+        self.reccomended_actions = PriorityQueue()
         self.X0v, self.rates, self.SB = GlucoseInitializer(self.__param, self).calculate_values()
         self.reset()
 
@@ -100,18 +105,20 @@ class T2DPatient(Patient):
 
     @property
     def state(self):
-        return self._odesolver.y[:, -1]
+        return self._odesolver.y
 
     @property
     def t(self):
-        return self._odesolver.t[-1]
+        return self._odesolver.t
 
     @property
     def sample_time(self):
         return self.SAMPLE_TIME
 
-    def step(self, action):
+    def step(self, action, reccomendation_action):
         # Convert announcing meal to the meal amount to eat at the moment
+
+
         to_eat = self._announce_meal(action.CHO)
         action = action._replace(CHO=to_eat)
 
@@ -135,27 +142,21 @@ class T2DPatient(Patient):
             self.is_eating = False
 
         if self._last_action.metformin != 0:
-            print("doing anything?")
             action = action._replace(metformin=0)
 
         # Update last input
         self._last_action = action
 
         # ODE solver
-        #self._odesolver.set_f_params(action, self.basal, self.__param, self)
-        if not hasattr(self, "_odesolver"):
-            self._odesolver = solve_ivp(fun=self.model, t_span=(self._odesolver.t ,self.t0+self.sample_time), y0=self._odesolver.y, method='RK45', args=(action, self.basal, self.__param, self))
-        elif self._odesolver.success:
-            print((self.t ,self.t+self.sample_time))
-            self._odesolver = solve_ivp(fun=self.model, t_span=(self.t ,self.t+self.sample_time), y0=self.state, method='RK45', args=(action, self.basal, self.__param, self))
-
-            #self._odesolver.integrate(self._odesolver.t + self.sample_time)
+        self._odesolver.set_f_params(action, self.basal, self.__param, self)
+        if self._odesolver.successful():
+            self._odesolver.integrate(self._odesolver.t + self.sample_time)
         else:
             logger.error('ODE solver failed!!')
             raise
 
     @staticmethod
-    def model(t, x, action, basal,glucose_parameters, self):
+    def model(t, x, action, basal, glucose_parameters, self):
         action = self._last_action
         Dg = action.CHO * 1e3
         long_insulin = action.insulin_long * 1e2
@@ -182,14 +183,8 @@ class T2DPatient(Patient):
         values = [Dg, Dg, DM, metformin, metformin, vildagliptin, fast_insulin / 6.76, long_insulin / 6.76]
 
         for index, value in zip(indices, values):
-            x[index] += value
+            x[index] = value + x[index]
         glucose_dynamics = GlucoseDynamics(t,x,Dg,stress,physical,basal,glucose_parameters)
-        
-    # # Solve the ODE using solve_ivp
-    # sol = solve_ivp(fun=ode_func, t_span=interval, y0=initialVec, method=integration)
-    
-    # ts = sol.t  # Time points
-    # Xs = sol.y  # Solution
         dxdt = glucose_dynamics.compute()
         self._last_action = action
         return dxdt
@@ -261,13 +256,10 @@ class T2DPatient(Patient):
        # self.name = self._params.Name
 
         ## should be fine but order is 4 (5) while other tested is order 5(4)
-        # self._odesolver = ode(self.model).set_integrator('dopri5', nsteps=500, max_step=0.1, rtol=0.001, atol=1e-06,first_step=0.01)
-        # self._odesolver.set_initial_value(self.X0v, self.t0)
-
+        self._odesolver = ode(self.model).set_integrator('dopri5')
+        self._odesolver.set_initial_value(self.X0v, self.t0)
 
         self._last_action = Action(CHO=0, insulin_fast=0, insulin_long=0, metformin=0, vildagliptin=0, stress=0, physical=0)
-        self._odesolver = solve_ivp(fun=self.model, t_span=(self.t0 ,self.t0+self.sample_time), y0=self.X0v, method='RK45', args=(Action(CHO=0, insulin_fast=0, insulin_long=0, metformin=0, vildagliptin=0, stress=0, physical=0), self.basal, self.__param, self))
-
         self.is_eating = False
         self.planned_meal = 0
 